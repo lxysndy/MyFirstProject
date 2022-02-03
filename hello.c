4f6dbe452c549af2e2f07f6253e2dffed3e48638
xy <1243024418@qq.com>
2022-02-03 19:51:03 +0800

    change

--- hello.c
+++ hello.c
@@ -1,5 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2022-02-03 19:26:03
+ * @LastEditTime: 2022-02-03 23:06:58
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \undefinedd:\MyGitHubFiles\MyFirstProject\hello.c
+ */
 #include <stdio.h>
-int main(){
-    printf("hello world!");
+int main()
+{
+    printf("hello hi");
     return 0;
 }
